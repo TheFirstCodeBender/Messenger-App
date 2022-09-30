2d1cbb9d79bc6481a6aedb22f1ad3f9c43298ad1
timDeHof <this.is.tim.dehof@gmail.com>
2022-09-30 18:27:40 -0400

    hooked up the login button with the google redirect from firebase utils

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { useContext } from "react";
 
 import Login from "./components/login/login.component";
+import { UserContext } from "./contexts/auth.context";
 import "./App.css";
 
 function App() {
+  const { user } = useContext(UserContext);
   return (
-
     <div className='App'>
+      <h1>{user}</h1>
       <Login />
     </div>
-
   );
 }
 
--- src/components/login/login.component.jsx
+++ src/components/login/login.component.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./login.styles.scss";
-
-const signInWithGoogle = () => {
-  console.log("clicked");
-};
+import { UserContext } from "../../contexts/auth.context";
+import { signInWithGoogleRedirect } from "../../utils/firebase.utils";
 
 const Login = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    console.log(user);
+    setUser(user);
+  };
   return (
     <div className='signIn-container'>
-      <h2>Sign In</h2>
+      {user ? <h2>Sign out</h2> : <h2>Sign In</h2>}
+      {console.log(user)}
       <button onClick={signInWithGoogle}>Google Sign In</button>
     </div>
   );
